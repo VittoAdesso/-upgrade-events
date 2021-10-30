3beccb6135244d3d842fc7bc7fd3663667db5764
Vittorio Adesso <adessovittorio8@gmail.com>
2021-10-30 20:33:48 +0200

    Iteración #2

---
+++ focus.js
@@ -0,0 +1,11 @@
+/* 1.2 Añade un evento 'focus' que ejecute un console.log con el valor del input. */
+
+
+
+function enfoque() {
+
+    document.getElementById("focus").style.background = "green";
+
+    console.log('He cambiado de color al enfocarme! Gracias');
+
+}
