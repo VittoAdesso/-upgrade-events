889c5e5154b061e9590b784623dad0bda336faba
Vittorio Adesso <adessovittorio8@gmail.com>
2021-10-30 20:24:48 +0200

    Iteración #3

---
+++ input.js
@@ -0,0 +1,9 @@
+/* 1.3 Añade un evento 'input'
+que ejecute un console.log con el valor del input. */
+
+
+function miraDentro() {
+    const resultado1 = document.getElementById('value').value; //insertando texto y danbdo click, me lee el input más me lo muestra por consola 
+    console.log(resultado1);
+
+}
